ee3715fff7b66ab3486542b90b8bb44f5cb2657b
akashmondal2000 <mondalakash494@gmail.com>
2026-04-17 23:29:47 +0530

    split Input components , style buttom and input fild

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,27 +1,18 @@
-import React from 'react'
+import Input from "./Input.jsx"
+
 
 const NewProject = () => {
   return (
-    <div>
-        <menu>
-            <li><button>Cancle</button></li>
-            <li><button>Save</button></li>
+    <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li><button className="text-gray-700 hover:text-gray-950">Cancle</button></li>
+            <li><button className=" py-2 px-4 rounded-md bg-gray-700 text-gray-200 hover:bg-gray-950 text-gray-100">Save</button></li>
         </menu>
 
         <div>
-            <p>
-                <label>Title</label>
-                <input />
-            </p>
-
-            <p>
-                <label>Description</label>
-                <textarea />
-            </p>
-            <p>
-                <label>Due Date</label>
-                <input />
-            </p>
+            <Input label="Title" type="text"/>
+            <Input label="Description" textarea={true} />
+            <Input label="Due Date" />
         </div>
     </div>
   )
